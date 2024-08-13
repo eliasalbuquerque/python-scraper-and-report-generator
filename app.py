82976395a7f02c2c0bd7f35e0f812b196a487357
Elias Albuquerque <78819295+eliasalbuquerque@users.noreply.github.com>
2024-08-13 15:20:30 -0300

    feat(app): added file conversion

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # title: 'app'
 # author: 'Elias Albuquerque'
-# version: '0.2.0'
+# version: '0.3.0'
 # created: '2024-08-08'
-# update: '2024-08-09'
+# update: '2024-08-10'
 
 
 import logging
@@ -11,77 +11,12 @@ import datetime
 import os
 import tempfile
 import shutil
-from docx.shared import Inches, Pt, RGBColor
-from docx.enum.text import WD_ALIGN_PARAGRAPH
-from docx.enum.style import WD_STYLE_TYPE
 from time import sleep
 from src.settings import Settings
 from src.website import Website
 from src.office import Office
+from src.report import report_content
 
-
-def keep_the_window_open():  # teste
-    logging.info('Mantendo site aberto por tempo indeterminado...')
-    input('')
-
-
-def report_content(word, quote, today, hour, url, screenshot, author):
-    """Adiciona o conteúdo do relatório ao documento existente."""
-
-    if word.doc is None:
-        logging.error("Erro ao criar o documento Word. O relatório não foi gerado.")
-        return
-
-    try:
-        # Adiciona o título
-        heading_text = "Cotação Atual do Dólar - " + quote + " (" + today + ")"
-        heading = word.doc.add_paragraph(heading_text, style='Heading 1')
-        heading.alignment = WD_ALIGN_PARAGRAPH.CENTER
-        
-        # Formatando o texto do título
-        run = heading.runs[0]
-        run.font.size = Pt(20)
-        run.font.color.rgb = RGBColor(0, 0, 0)  # Preto
-        heading.paragraph_format.space_after = Pt(24)
-
-        # Cria um estilo de parágrafo personalizado
-        styles = word.doc.styles
-        paragraph_style = styles.add_style('MyParagraphStyle', WD_STYLE_TYPE.PARAGRAPH)
-        paragraph_style.font.size = Pt(12)  # Define o tamanho da fonte para 12pt
-        paragraph_style.paragraph_format.space_after = Pt(0)  # No Spacing
-        paragraph_style.paragraph_format.space_before = Pt(0)  # No Spacing
-        paragraph_style.paragraph_format.line_spacing = Pt(12)  # Define o espaçamento entre linhas para 12 pt
-
-        # Adiciona os parágrafos usando o estilo personalizado
-        paragraph = word.doc.add_paragraph("O dólar está no valor de " + quote, style='MyParagraphStyle')
-        paragraph.add_run(", na data " + today + " às " + hour)
-        
-        word.doc.add_paragraph("Valor cotado no site " + url, style='MyParagraphStyle')
-        word.doc.add_paragraph("Print da cotação atual:", style='MyParagraphStyle')
-        
-        # Adiciona um parágrafo em branco para o espaçamento antes da imagem
-        paragraph_blank = word.doc.add_paragraph()
-        paragraph_blank.paragraph_format.space_before = Pt(7)
-        
-        # Verifica se a imagem existe antes de tentar adicionar
-        if os.path.exists(screenshot):
-            picture = word.doc.add_picture(screenshot, width=Inches(5.88))
-        else:
-            logging.error(f"Imagem não encontrada: {screenshot}")
-        
-        # Insere espaço apos a imagem
-        paragraph_blank.paragraph_format.space_before = Pt(7)
-        word.doc.add_paragraph("Cotação feita por: " + author, style='MyParagraphStyle')
-
-        # Salva o documento após adicionar todo o conteúdo
-        file_path = os.path.join('reports', 'teste.docx')
-        word.doc.save(file_path)
-
-        logging.info(f"Conteúdo adicionado ao arquivo e salvo em: .\\{file_path}")
-
-    except Exception as e:
-        logging.error(f"Erro ao adicionar conteúdo ao documento: {e}")
-    
 
 def get_current_date_time():
     now = datetime.datetime.now()
@@ -93,57 +28,71 @@ def load_config():
         return json.load(file)
 
 
+def string_to_float_to_string(value):
+    """
+    Converte uma string para um valor float com duas casas decimais.
+    """
+
+    value = value.replace(",", ".")
+    float_value = float(value)
+    float_value_rounded = round(float_value, 2)
+    value_to_string = str(float_value_rounded)
+    value_rounded = value_to_string.replace(".", ",")
+
+    return value_rounded
+
+
 def main():
-    settings = Settings()
-    website = Website(settings)
-    word = Office()
-
-
-    # Criar a pasta temporária
+    # Criar a pasta temporária para salvar o screenshot
     tempdir = tempfile.mkdtemp()
 
     try:
         # 0. Carrega as configuracoes e variaveis da aplicacao
+        settings = Settings()
         config = load_config()
         url = config['website']['url']
         xp_button_cookie = config['website']['xp_button_cookie']
         xp_quote = config['website']['xp_quote']
-
-        # 1. Acessar o site e extrair o valor da cotacao
-        website.access_website(url)
-        website.click_on_element(xp_button_cookie)
-        website.zoom_out_of_website(78)
-        
-        quote = website.extract_text_from_element(xp_quote, 'cotação')
-        screenshot = website.take_screenshot(tempdir)
-
-        # 2. Criar arquivo Word.docx montar o relatorio
-        word.create_document('teste.docx', 'reports')
-    
-        # 3. Adicionar conteudo no relatorio
-        today = get_current_date_time()
         now = get_current_date_time()
         today = now.strftime("%d/%m/%Y")
         hour = now.strftime("%H:%M:%S")
         author = config['office']['author']
         if author == "null":
             author = os.getlogin().capitalize()
+        report_file = "relatorio-" + now.strftime("%Y%m%d-%H%M%S")
+        report_path = os.path.join('reports', report_file)
 
-        report_content(word, quote, today, hour, url, screenshot, author)
 
-        # * alterar o valor quote para um numero float
-        # * adicionar R$ na string do titulo e demais paragrafos
+        # 1. Acessar o site e extrair o valor da cotacao
+        website = Website(settings)
+        website.access_website(url)
+        website.click_on_element(xp_button_cookie)
+        website.zoom_out_of_website(86)
+    
+        quote = website.extract_text_from_element(xp_quote, 'cotação')
+        screenshot = website.take_screenshot(tempdir)
+    
+
+        # 2. Criar arquivo Word.docx montar o relatorio
+        office = Office()
+        office.create_document(report_file, report_path)
+
+
+        # 3. Adicionar conteudo no relatorio
+        quote = "R$ " + string_to_float_to_string(quote)
+        report_content(office, report_path, quote, today, hour, url, screenshot, author)
+
+
+        # 4. Transforme em um PDF
+        office.convert_docx_to_pdf(report_path)
+
+
         # * revise as docstrings de todos os modulos
-        # * transformar funcao report_content() em um modulo
         # * informar que o modulo report_content é necessario refatorar o codigo caso seja reutilizado em outro projeto
 
-        # 3. Transforme em um PDF
-        # - Encontre uma forma de transformar o arquivo word em um arquivo python, usando somente python e transforme o arquivo word em um arquivo pdf
-        # 4. Entrega como executável
-        # - Transforme o código em um instalador(instruções nas dicas abaixo)
 
-        # keep_the_window_open()
-        # os.rmdir(tempdir)
+        # 5. Entrega como executável
+        # - Transforme o código em um instalador(instruções nas dicas abaixo)
 
     finally:
         # Remover a pasta temporária no final da execução, se necessário
